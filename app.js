d6b9eb05a37a388238b3b69869e90927d6990c15
brianleroux <b@brian.io>
2013-05-19 18:06:08 -0700

    ok that was worth trying

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var app = require('./lib/configuration')
 ,   auth = require('./lib/authenticated')
+,   request = require('request')
+,   https = require('https')
+,   fs = require('fs')
 
 app.get('/', function(req, res){
     res.render('index', { user: req.user })
 })
 
 app.get('/account', auth, function(req, res){
-    res.render('account', { user: req.user })
+    var token = req.session.token
+/*
+GET /mirror/v1/timeline HTTP/1.1
+    Host: www.googleapis.com
+    Authorization: Bearer {auth token}
+    var options = {
+        uri:"https://www.googleapis.com/mirror/v1/timeline",
+        headers:{"Authorization": "Bearer " + token}
+    }
+    
+    request(options, function(err, data, str) {
+        console.log("\n\n\n\n\n")
+        console.log(data)
+        console.log("\n\n\n\n\n")
+        res.render('account', { user: req.user, data:str  })
+    })
+*/
+        res.render('account', { user:req.user, data:req.session.token })
 })
 
 app.post('/message', auth, function(req, res) {
@@ -19,4 +39,12 @@ app.get('/login', function(req, res){
     res.render('login', { user: req.user })
 })
 
-app.listen(3000)
+
+
+var privateKey = fs.readFileSync(__dirname + '/key.pem').toString();
+var certificate = fs.readFileSync(__dirname + '/cert.pem').toString();
+var options = { key:privateKey, cert:certificate }
+// app.get('port')
+https.createServer(options,app).listen(3000, function(){
+      console.log("Express server listening on port " + app.get('port'));
+})
---
+++ foo.js
@@ -0,0 +1,1 @@
+console.log("POST /o/oauth2/token HTTP/1.1\r\nhost: accounts.google.com\r\ncontent-type: application/x-www-form-urlencoded; charset=utf-8\r\ncontent-length: 282\r\nConnection: keep-alive\r\n\r\n")
